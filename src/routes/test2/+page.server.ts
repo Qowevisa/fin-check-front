086654a1ecdb2d724e7934d113c21dbe9b866195
qowevisa <qowevisa@gmail.com>
2024-11-30 16:41:45 +0200

    Delete test2 page

--- src/routes/test2/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async ({ fetch }) => {
-  const response = await fetch("https://api.fin.qowevisa.click/api/authping");
-
-  return {
-    message: await response.json()
-  };
-}
